80305d19e1c2a00717a09b44f7967ef36ef510a0
jan <jan.rocnik@gmail.com>
2012-11-12 23:19:16 +0100

    new edit system working
    old system has been removed

--- pflegeadmin/views.py
+++ pflegeadmin/views.py
@@ -33,93 +33,39 @@ def familyDetails(request, family_id):
       context_instance=RequestContext(request))
 
 @login_required
-def familyEdit(request, family_id):
-  family = get_object_or_404(Family, pk=family_id)
-  return render_to_response('pflegeadmin/familyEdit.html',
-      {'family' : family},
-      context_instance=RequestContext(request))
-
-@login_required
-def familySaveExisting(request, family_id):
-  family = get_object_or_404(Family, pk=family_id)
-  family_id = __SaveFamilyData(request, family)
-  return HttpResponseRedirect('/pflegeadmin/f/'+family_id)
-
-@login_required
-def familySaveNew(request):
-  family = Family()
-  family_id = __SaveFamilyData(request, family)
-  return HttpResponseRedirect('/pflegeadmin/f/'+family_id)
-
-@login_required
 def familyDelete(request, family_id):
   family = get_object_or_404(Family, pk=family_id)
   family.delete()
   return HttpResponseRedirect('/pflegeadmin/f/')
 
 @login_required
-def familyAdd(request):
-  return render_to_response('pflegeadmin/familyAdd.html',
-      {},
-      context_instance=RequestContext(request))
-
-@login_required
 def familyUpdateForm(request, family_id):
-
+  
   family = get_object_or_404(Family, pk=family_id)
 
-
   if request.method == 'POST':
-    form = FamilyForm(request.POST) # A form bound to the POST data
+    form = FamilyForm(request.POST, instance=family)
     if form.is_valid():
-      # All validation rules pass
-      # Process the data in form.cleaned_data
-      # ...
       family = form.save()
-      #family.id=family_id
-      #family.save()
       return HttpResponseRedirect('/pflegeadmin/f/'+str(family.id))
   else:
     form = FamilyForm(instance=family)
 
-  return render(request, 'pflegeadmin/familyForm.html',{'form': form, 'family' : family, })
+  return render_to_response('pflegeadmin/familyForm.html',
+    {'form': form, 'family' : family, },
+    context_instance=RequestContext(request))
 
 @login_required
 def familyCreateForm(request):
 
   if request.method == 'POST':
-    form = FamilyForm(request.POST) # A form bound to the POST data
+    form = FamilyForm(request.POST)
     if form.is_valid():
-      # All validation rules pass
-      # Process the data in form.cleaned_data
-      # ...
       family = form.save()
-      family.save()
       return HttpResponseRedirect('/pflegeadmin/f/'+str(family.id))
   else:
     form = FamilyForm()
 
-  return render(request, 'pflegeadmin/familyForm.html',{'form': form, })
-
-
-def __SaveFamilyData(request, family):
-  family.firstname_care_p = request.POST.get('firstname_care_p',family.firstname_care_p)
-  family.lastname_care_p = request.POST.get('lastname_care_p',family.lastname_care_p)
-  family.firstname_contact_p = request.POST.get('firstname_contact_p',family.firstname_contact_p)
-  family.lastname_contact_p = request.POST.get('lastname_contact_p',family.lastname_contact_p)
-  #family.phone_contact_p = request.POST.get('phone_contact_p',family.phone_contact_p)
-  #family.phone_care_p = request.POST.get('phone_care_p',family.phone_care_p)
-  #family.address_street = request.POST.get('address_street',family.address_street)
-  #family.address_number = request.POST.get('address_number',family.address_number)
-  #family.address_postal = request.POST.get('address_postal',family.address_postal)
-  #family.address_town = request.POST.get('address_town',family.address_town)
-  foo = request.POST.get('care_level','0')
-  print foo
-
-  try: bar = int(request.POST.get("care_level", '1'))
-  except ValueError: bar = 0
-  print bar
-  family.care_level = int(bar)
-  #family.date_of_birth = request.POST.get('date_of_birth',family.date_of_birth)
-  family.save()
-  return str(family.id);
+  return render_to_response('pflegeadmin/familyForm.html',
+    {'form': form, },
+    context_instance=RequestContext(request))
